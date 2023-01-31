356465b90bfaed03cc2247f2438480621716ab2c
Виталий <109619045+Vitaliy5796@users.noreply.github.com>
2023-01-31 16:44:08 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,25 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean compareHelloWorld = bean == secondBean;
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+
+        Cat secondCatBean = (Cat) applicationContext.getBean("cat");
+
+        boolean compareCat = catBean == secondCatBean;
+
+        System.out.println(compareHelloWorld);
+        System.out.println(compareCat);
+    }
+}
